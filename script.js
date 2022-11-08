5663662dce2f098e3bd14c8cc31f339bb0357c9d
FarisMuhovic <faris.fare123456@gmail.com>
2022-11-08 22:37:37 +0100

    func add

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 let clickedup = false;
 let clickeddown = false;
+let count = localStorage.getItem("count");
+if (count == null) {
+  count = 0;
+}
 function newreply(event) {
-  event.target.parentNode.parentNode.parentNode.innerHTML += `
-  <div class="comment" style="margin-left: 10%">
+  count++;
+  let replyTemplate = `
+  <div class="comment" style="margin-left: 10%" id="replies" data-del=${count}>
     <div class="user">
       <img src="https://i.kym-cdn.com/entries/icons/original/000/028/861/cover3.jpg" alt="avatar">
       <h2>Faris Muhovic</h2>
     </div>
-    <textarea type="text" id="textinput" maxlength="569"></textarea>
+    <textarea type="text" id="textinput" maxlength="569" ></textarea>
     <div class="review">
       <button onclick="thumbsup(event)"><i class="fa-solid fa-thumbs-up"></i> <textarea disabled="true">0</textarea></button>
       <button onclick="thumbsdown(event)"><i class="fa-solid fa-thumbs-down"></i> <textarea disabled="true">0</textarea></button>
       <button id="reply" onclick="newreply(event)"><i class="fa-solid fa-reply"></i></button>
       <button id="post" onclick="post(event)">Post</button>
+      <button id="remove"><i class="fa-solid fa-trash" onclick="deleteReply(event)"></i></button>
     </div>
   </div>`;
+  localStorage.setItem(count, replyTemplate);
+  localStorage.setItem("count", count);
+  event.target.parentNode.parentNode.parentNode.innerHTML += replyTemplate;
   clickedup = false;
   clickeddown = false;
 }
-
+let collapsed = false;
+function collapse() {
+  if (collapsed == false) {
+    let singleReply = document.querySelectorAll("#replies");
+    singleReply.forEach(reply => {
+      reply.style.display = "none";
+    });
+    document.getElementById(
+      "collapse"
+    ).innerHTML = `<i class="fa-solid fa-square-plus"></i>`;
+    collapsed = true;
+  } else {
+    let singleReply = document.querySelectorAll("#replies");
+    singleReply.forEach(reply => {
+      reply.style.display = "flex";
+    });
+    document.getElementById(
+      "collapse"
+    ).innerHTML = `<i class="fa-solid fa-square-minus"></i>`;
+    collapsed = false;
+  }
+}
 function post(event) {
   let textbox = event.target.parentNode.parentNode.children[1];
   let textboxvalue = textbox.value;
@@ -54,7 +84,12 @@ function thumbsdown(event) {
     clickeddown = false;
   }
 }
-// be able to collapse elements
-// save it to local storage as "server"
+function deleteReply(event) {
+  let container = event.target.parentNode.parentNode.parentNode;
+  container.remove();
+  localStorage.setItem(container.getAttribute("data-del"), "");
+}
 // make it so u cant make another post while making the post and prevent spam for creating posts.
-// make a diffrent post for getting post making smth like that
+for (let i = 1; i < localStorage.length; i++) {
+  document.getElementById("commcont").innerHTML += localStorage.getItem(i);
+}
